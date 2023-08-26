705a40d0513a981eba796f6078380b180b972fb3
Eugene test <eupushkash@ngs.ru>
2023-08-26 12:21:19 +0300

    Last commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ public class Main {
         int chek = 100;
         int topup = 1100;
         int bonus;
-        if (topup > 0) {
+        if (topup > 1000) {
             bonus = topup / 100;
-            int balance = bonus + chek + topup;
-            System.out.println("Баланс счёта = " + balance + " рублей");
-            System.out.println("Бонусов = " + bonus+ " рублей");
+        } else {
+            bonus = 0;
         }
+        int balance = bonus + chek + topup;
+        System.out.println("Баланс счёта = " + balance + " рублей");
+        System.out.println("Бонусов = " + bonus + " рублей");
     }
 }
